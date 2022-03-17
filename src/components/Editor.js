c04015fdfecf9538bfa79cf7464ab76dd6ef827e
i-naeem <inaeem@protonmail.com>
2022-03-17 19:25:25 +0500

    :art: update editor styles

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,3 +1,15 @@
+import styled from 'styled-components';
+
+const StyledTextArea = styled('textarea')`
+  width: 100vw;
+  height: 100%;
+  padding: 5px;
+  border: none;
+  resize: none;
+  outline: none;
+  font-size: 13px;
+  font-family: 'Consolas', 'Lucida Sans', 'sans-serif';
+`;
 export const Editor = (props) => {
-  return <textarea></textarea>;
+  return <StyledTextArea {...props} />;
 };
