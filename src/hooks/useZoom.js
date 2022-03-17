6f4cff3a4b8146c246fad8f63b347f8bb29a862a
i-naeem <inaeem@protonmail.com>
2022-03-17 19:37:57 +0500

    :memo: give credit to John Au-Yeung for zoom level detection code snippet

--- src/hooks/useZoom.js
+++ src/hooks/useZoom.js
@@ -3,6 +3,10 @@ import { useEffect, useState } from 'react';
 export const useZoom = () => {
   const [zoom, setZoom] = useState(100);
   useEffect(() => {
+    /* 
+      Below code snippet is taken from https://thewebdev.info/2021/03/10/how-to-detect-page-zoom-levels-in-modern-browsers/ article wrritten by John Au-Yeung. Just giving credit to the man as this solution worked for me.
+     */
+
     window.addEventListener('resize', () => {
       const browserZoomLevel = Math.round(window.devicePixelRatio * 100);
       setZoom(browserZoomLevel);
